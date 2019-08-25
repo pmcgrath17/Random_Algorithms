9f793a536ee7477b5853494b0cedbafce5173c05
pmcgrath17 <patarmcgrath17@gmail.com>
2019-08-25 11:20:55 -0700

    Edits to heap_sort algorithm

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -19,10 +19,11 @@ void percolate(Vector vector, int last_index);
 
 int main(){
 
-	int array [] = {2, 20, 1, 7, 8, 9, 12};
-	Vector vector(array, 7);
+	int array [] = {2, 20, 1, 7, 8, 9, 12, 6, 30};
+	Vector vector(array, 9);
 	vector.print_vector();
-
+	heap_sort(vector);
+	vector.print_vector();
 	vector.free_vector();
 }
 
@@ -30,20 +31,80 @@ int main(){
 void heap_sort(Vector vector){
 // need to percolate and then switch
 
-/*
-	for(int i = vector.size; i > 0; --i){
-		int temp = vector.at(i);
+	for(int i = vector.size - 1; i > 0; --i){
+		percolate(vector, i);
+		int temp = vector.data[i];
 		vector.data[i] = vector.data[0];
 		vector.data[0] = temp;
-		percolate(vector, i);
+		cout << "the first and last indexes have been switched so now the array is:" << endl;
+		vector.print_vector();
 	}
-*/
 
 }
 
 void percolate(Vector vector, int last_index){
 
+	int j = 0;
+	do {
 
+		if(vector.data[j] == -10){
+			return;
+		}
+		cout << "======= inside do/while loop =========" << endl;
+		vector.print_vector();
+		int left_child = 2*j + 1;
+		int right_child = 2*j + 2;
+
+		if(left_child <= last_index && right_child <= last_index){
+
+			if(vector.data[left_child] > vector.data[right_child]){
+				if(vector.data[j] < vector.data[left_child]){
+
+					int temp = vector.data[j];
+					vector.data[j] = vector.data[left_child];
+					vector.data[left_child] = temp;
+					j = left_child;
+
+				} else {
+					return;
+				}
+
+			} else {
+
+				if(vector.data[j] < vector.data[right_child]){
+
+					int temp = vector.data[j];
+					vector.data[j] = vector.data[right_child];
+					vector.data[right_child] = temp;
+					j = right_child;
+					cout << " j is now equal to the right_child: " << j;
+
+				} else {
+					return;
+				}
+
+			}
+
+		} else if(left_child <= last_index){
+
+			if(vector.data[j] < vector.data[left_child]){
+
+				int temp = vector.data[j];
+				vector.data[j] = vector.data[left_child];
+				vector.data[left_child] = temp;
+				j = left_child;
+
+			} else {
+				return;
+			}
+
+		} else {
+			return;
+		}
+
+		cout << "j's value is: " << j << " with value " << vector.data[j] << endl;
+
+	} while(j <= last_index);
 }
 
 
@@ -52,8 +113,12 @@ Vector::Vector(int array[], int size){
 	this->size = size;
 	this->capacity = size*2;
 	this->data = new int[capacity];
-	for(int i = 0; i < size; ++i){
-		this->data[i] = array[i];
+	for(int i = 0; i < capacity; ++i){
+		if(i < size){
+			this->data[i] = array[i];
+		} else {
+			this->data[i] = -10;
+		}
 	}
 
 }
